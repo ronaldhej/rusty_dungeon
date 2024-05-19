2384c0cf6bcc56a8ae5c4462a5919085957b372e
ronaldj <ronaldhej@gmail.com>
2024-05-19 11:16:17 +0200

    json not mapping correctly to the map type, raw json is imported though

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,28 @@
-use bevy::{prelude::*, ui::UI_MATERIAL_SHADER_HANDLE};
+use bevy::{
+    log::tracing_subscriber::fmt::FormattedFields, prelude::*, ui::UI_MATERIAL_SHADER_HANDLE,
+};
 use bevy_egui::{egui, EguiContexts, EguiPlugin};
-
-#[derive(Component)]
-struct Player;
+use serde::{ser::Error, Deserialize, Serialize};
+use std::{borrow::Borrow, process::Command};
 
 #[derive(Component)]
 struct MyApp {
     selected_file_input: SelectedPathInput,
     binary_path: Option<String>,
     script_path: Option<String>,
-    output_path: Option<String>,
+    python_path: Option<String>,
+    map: Option<Map>,
 }
 
 #[derive(PartialEq)]
 enum SelectedPathInput {
+    Python,
     Binary,
     Script,
-    Output,
 }
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Map(Vec<Vec<char>>);
 
 fn main() {
     App::new()
@@ -30,10 +35,11 @@ fn main() {
 
 fn setup(mut commands: Commands) {
     commands.spawn(MyApp {
-        selected_file_input: SelectedPathInput::Binary,
+        selected_file_input: SelectedPathInput::Python,
         binary_path: None,
         script_path: None,
-        output_path: None,
+        python_path: None,
+        map: None,
     });
 }
 
@@ -41,25 +47,24 @@ fn file_picker_system(mut contexts: EguiContexts, mut query: Query<&mut MyApp>) 
     let mut my_app = query.single_mut();
 
     egui::Window::new("Choose file").show(contexts.ctx_mut(), |ui| {
-
         ui.label("Which file would you like to select?");
         //Radio menu
         ui.horizontal(|ui| {
-        ui.radio_value(
-            &mut my_app.selected_file_input,
-            SelectedPathInput::Binary,
-            "Binary",
-        );
-        ui.radio_value(
-            &mut my_app.selected_file_input,
-            SelectedPathInput::Script,
-            "Script",
-        );
-        ui.radio_value(
-            &mut my_app.selected_file_input,
-            SelectedPathInput::Output,
-            "Output",
-        );
+            ui.radio_value(
+                &mut my_app.selected_file_input,
+                SelectedPathInput::Python,
+                "Python",
+            );
+            ui.radio_value(
+                &mut my_app.selected_file_input,
+                SelectedPathInput::Binary,
+                "Binary",
+            );
+            ui.radio_value(
+                &mut my_app.selected_file_input,
+                SelectedPathInput::Script,
+                "Script",
+            );
         });
         ui.label("Select a file");
 
@@ -73,13 +78,46 @@ fn file_picker_system(mut contexts: EguiContexts, mut query: Query<&mut MyApp>) 
                     SelectedPathInput::Script => {
                         my_app.script_path = Some(path.display().to_string())
                     }
-                    SelectedPathInput::Output => {
-                        my_app.output_path = Some(path.display().to_string())
+                    SelectedPathInput::Python => {
+                        my_app.python_path = Some(path.display().to_string())
                     }
                     _ => println!("Invalid radio option"),
                 }
             }
         }
+
+        if ui.button("Run binary").clicked() {
+            match (
+                &my_app.python_path,
+                &my_app.binary_path,
+                &my_app.script_path,
+            ) {
+                (Some(python_path), Some(binary_path), Some(script_path)) => {
+                    let result: Result<String, std::string::FromUtf8Error> =
+                        run_binary(python_path, binary_path, script_path);
+                    match result {
+                        Ok(result) => {
+                            println!("result: {result}");
+                            let _map = match parse_output_to_struct(&result) {
+                                Ok(map) => my_app.map = Some(map),
+                                Err(e) => println!("Error: {e}"),
+                            };
+                        }
+                        Err(e) => println!("Error: {e}"),
+                    }
+                }
+                _ => {
+                    println!("Some paths are missing, script cannot be run")
+                }
+            }
+        }
+
+        if let Some(python_path) = &my_app.python_path {
+            ui.horizontal(|ui| {
+                ui.label("Python file:");
+                ui.monospace(python_path);
+            });
+        };
 
         if let Some(binary_path) = &my_app.binary_path {
             ui.horizontal(|ui| {
@@ -87,17 +125,50 @@ fn file_picker_system(mut contexts: EguiContexts, mut query: Query<&mut MyApp>) 
                 ui.monospace(binary_path);
             });
         };
+
         if let Some(script_path) = &my_app.script_path {
             ui.horizontal(|ui| {
                 ui.label("Script file:");
                 ui.monospace(script_path);
             });
         };
-        if let Some(output_path) = &my_app.output_path {
+
+        if let (Some(binary_path), Some(script_path)) = (&my_app.binary_path, &my_app.script_path) {
             ui.horizontal(|ui| {
-                ui.label("Picked file:");
-                ui.monospace(output_path);
+                ui.label("Command to be run: ");
+                ui.monospace(format!("python3 {binary_path} -p {script_path}"))
             });
-        };
+        }
     });
+}
+
+fn run_binary(
+    python_path: &str,
+    binary_path: &str,
+    script_path: &str,
+) -> Result<String, std::string::FromUtf8Error> {
+    let output = if cfg!(target_os = "windows") {
+        Command::new("cmd")
+            .args(["/C", "echo hello"])
+            .output()
+            .expect("failed to execute process")
+    } else {
+        Command::new(python_path)
+            .arg(binary_path)
+            .arg("-p")
+            .arg(script_path)
+            .output()
+            .expect("failed to execute process")
+    };
+
+    if output.status.success() {
+        return String::from_utf8(output.stdout);
+    } else {
+        return String::from_utf8(output.stderr);
+    }
+}
+
+fn parse_output_to_struct(output: &str) -> Result<Map, serde_json::Error> {
+    let result: Result<Map, serde_json::Error> = serde_json::from_str(output);
+    return result;
 }
