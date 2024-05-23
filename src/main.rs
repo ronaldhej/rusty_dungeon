2c1fa53ecc89053735990b25ba048e42d189d5eb
ronaldj <ronaldhej@gmail.com>
2024-05-23 16:14:46 +0200

    Added zoom, pan and improved performance

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
+use bevy::input::mouse::{MouseMotion, MouseWheel};
+use bevy::render::camera::OrthographicProjection;
 use bevy::{prelude::*, render::view::ExtractedWindows, ui::UI_MATERIAL_SHADER_HANDLE};
 use bevy_egui::{egui, EguiContexts, EguiPlugin};
 use serde::{ser::Error, Deserialize, Serialize};
 use serde_json::Value;
 use std::{borrow::Borrow, collections::HashMap, process::Command};
 
-const TILE_SIZE: f32 = 32.0;
+const TILE_SIZE: f32 = 8.0;
 
 #[derive(Component)]
 struct MyApp {
@@ -13,6 +15,12 @@ struct MyApp {
     script_path: Option<String>,
     python_path: Option<String>,
     map: Option<HashMap<String, Room>>,
+}
+
+#[derive(Clone, Eq, PartialEq, Debug, Hash, States)]
+enum AppState {
+    Idle,
+    DrawTerrain,
 }
 
 #[derive(PartialEq)]
@@ -36,9 +44,12 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_plugins(EguiPlugin)
+        .insert_state(AppState::Idle)
         .add_systems(Startup, setup)
         .add_systems(Update, file_picker_system)
-        .add_systems(Update, draw_terrain_system)
+        .add_systems(Update, camera_movement)
+        .add_systems(Update, camera_zoom)
+        .add_systems(OnEnter(AppState::DrawTerrain), draw_terrain_system)
         .run();
 }
 
@@ -54,7 +65,44 @@ fn setup(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
 }
 
-fn file_picker_system(mut contexts: EguiContexts, mut query: Query<&mut MyApp>) {
+fn camera_movement(
+    mut query: Query<&mut Transform, With<Camera>>,
+    mouse_button_input: Res<ButtonInput<MouseButton>>,
+    mut mouse_motion_events: EventReader<MouseMotion>,
+) {
+    let mut camera_transform = query.single_mut();
+
+    if mouse_button_input.pressed(MouseButton::Right) {
+        for event in mouse_motion_events.read() {
+            camera_transform.translation.x -= event.delta.x;
+            camera_transform.translation.y += event.delta.y;
+        }
+    }
+}
+
+fn camera_zoom(
+    mut query: Query<(&mut OrthographicProjection, &mut Transform), With<Camera>>,
+    mut scroll_events: EventReader<MouseWheel>,
+) {
+    let (mut orthographic_projection, mut transform) = query.single_mut();
+
+    for event in scroll_events.read() {
+        orthographic_projection.scale -= event.y * 0.1;
+        orthographic_projection.scale = orthographic_projection.scale.clamp(0.1, 5.0);
+        // Adjust the camera translation to ensure zooming is centered on the current view
+        transform.scale = Vec3::new(
+            orthographic_projection.scale,
+            orthographic_projection.scale,
+            1.0,
+        );
+    }
+}
+
+fn file_picker_system(
+    mut contexts: EguiContexts,
+    mut query: Query<&mut MyApp>,
+    mut app_state: ResMut<NextState<AppState>>,
+) {
     let mut my_app = query.single_mut();
 
     egui::Window::new("Choose file").show(contexts.ctx_mut(), |ui| {
@@ -111,6 +159,7 @@ fn file_picker_system(mut contexts: EguiContexts, mut query: Query<&mut MyApp>) 
                             let (name, room) = parse_output_to_room(&result);
                             my_app.map.as_mut().unwrap().insert(name, room);
                             println!("Hashmap values: {:?}", my_app.map);
+                            app_state.set(AppState::DrawTerrain);
                         }
                         Err(e) => println!("Error: {e}"),
                     }
@@ -178,6 +227,7 @@ fn run_binary(
 }
 
 fn parse_output_to_room(output: &str) -> (String, Room) {
+    println!("Raw string: {}", output);
     let value: Value = serde_json::from_str(output).unwrap();
     let (room_name, room_content) = value
         .as_object()
@@ -193,6 +243,7 @@ fn parse_output_to_room(output: &str) -> (String, Room) {
 fn draw_terrain_system(
     mut commands: Commands,
     query: Query<&MyApp>,
+    mut app_state: ResMut<NextState<AppState>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
     let my_app = query.single();
@@ -206,6 +257,8 @@ fn draw_terrain_system(
                     let color = match cell.as_str() {
                         "#" => Color::rgb(1.0, 0.0, 0.0), // Red for "*"
                         "." => Color::rgb(0.8, 0.8, 0.8), // Green for "."
+                        "&" => Color::rgb(0.1, 0.7, 0.5), // Green for "."
+                        "@" => Color::rgb(0.1, 0.2, 0.3), // Green for "."
                         _ => Color::rgb(1.0, 1.0, 1.0),   // White for any other symbol
                     };
 
@@ -226,4 +279,5 @@ fn draw_terrain_system(
             }
         }
     }
+    app_state.set(AppState::Idle);
 }
